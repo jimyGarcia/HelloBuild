674f87c94e5bb36c229217bd6147c7e0f9bad9bb
jimy garcia <jsgarciab9@gmail.com>
2022-07-10 15:31:51 -0500

    Creating pages and Layouts

---
+++ src/pages/RepositoriesPage.js
@@ -0,0 +1,9 @@
+const RepositoriesPage = () => {
+    return (
+        <div>
+            <h1>RepositoriesPage</h1>
+        </div>
+    )
+};
+
+export default RepositoriesPage;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Layout from '../components/Layouts/Layouts';
+import AccountPage from '../pages/AccountPage';
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import NotFoundPage from '../pages/NotFoundPage';
+import RegisterPage from '../pages/RegisterPage';
+import RepositoriesPage from '../pages/RepositoriesPage'
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route exact path='/login' component={LoginPage} />
+                    <Route exact path='/register' component={RegisterPage} />
+                    <Route exact path='/account' component={AccountPage} />
+                    <Route exact path='/repositories' component={RepositoriesPage} />
+
+                    <Route exact path='*' component={NotFoundPage} />
+                </Switch>
+                
+                <h1>Mientras</h1>
+            </Layout>
+        </Router>
+    )
+};
